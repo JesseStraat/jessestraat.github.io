33567fc103ce5d99354e1f6310d0755540d5a2eb
Jesse Straat <jessestraat1@gmail.com>
2025-04-30 20:13:04 +0200

    Prevented overflows (but broke tags)

---
+++ _plugins/wrap_tables.rb
@@ -0,0 +1,12 @@
+[:posts, :pages].each do |hook|
+    Jekyll::Hooks.register hook, :post_render do |item|
+      if item.output_ext == ".html"
+        content = item.output
+        # Wrap <table> tags with a div with style="overflow-x:auto;"
+        content.gsub!(/<table(.*?)>/m, '<div style="overflow-x:auto;"><table\1>')
+        content.gsub!(/<\/table>/m, '</table></div>')
+        # Update the item content
+        item.output = content
+      end
+    end
+  end
